d4d5f0359b61a574b5f68ce4eccf3a963883f9fb
takaaki.konaka <takaaki.konaka@gaiax.com>
2016-05-25 16:17:27 +0900

    編集削除用のモーダルウィンドウを作成および表示

---
+++ static/js/timeline.js
@@ -0,0 +1,16 @@
+if(window.addEventListener){
+  window.addEventListener('load',init,false);
+}
+
+function init(){
+  var contents = document.getElementsByClassName("delete");
+  for(var i=0;i<contents.length;i++){
+    var post = contents[i];
+  }
+}
+
+
+
+function popup_screen(i){
+
+}
